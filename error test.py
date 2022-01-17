1e6add6fc6fefcc31e3a8bec3598916700867eed
oof1718963 <75444840+oof1718963@users.noreply.github.com>
2022-01-17 11:27:59 -0500

    Add files via upload

---
+++ error test.py
@@ -0,0 +1,54 @@
+e = ["blah", "help", "what"]
+try:
+    print(e[4])
+except:
+    print("help help help")
+    
+    
+    
+sussy = {"stonks" : "e",
+         "bruh" : "haha",}
+
+try:
+    print(sussy["jkwdk"])
+except:
+    print("hi hi hi")
+    
+    
+    
+    
+    
+from tkinter import *
+
+root = Tk()
+root.geometry("600x400")
+
+e = ["bruh", "qwwwwdertyui", "qwerty"]
+sussy = {"stonks" : "e",
+         "bruh" : "haha",}
+
+try:
+    print(e[1234]) 
+    print(sussy["when inposter sus"]) 
+except IndexError:
+    messagebox.showinfo("Warning", "Your device has a virus now")
+except KeyError:
+    messagebox.showinfo("Error", "There is now virus")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+root.mainloop()
